db4871fa6d786af62d0886f79d86cccdb111ee47
Sophia Wassermann <so.wassermann@gmail.com>
2017-08-11 11:23:08 +0100

    Updated for ggplot v2.2.0 where axes lines are now specified together

--- ggplot_linegraph.R
+++ ggplot_linegraph.R
@@ -4,14 +4,12 @@ library(ggplot2)
 
 ggplot() + 
   theme_bw() + 
-  geom_line(data= , aes(x= , y= ), colour="blue", size = 2) + #line
-  geom_point(data= , aes(x= , y= ), colour="black", size = 3) + #points on the line
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + #no gridlines
-  theme(axis.text.y = element_text(size = 12), #axis text size
+  geom_line(data= , aes(x= , y= ), colour="blue", size = 2) +  # line
+  geom_point(data= , aes(x= , y= ), colour="black", size = 3) +  # points on the line
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
+  theme(axis.text.y = element_text(size = 12),  # axis text size
         axis.text.x = element_text(size = 12)) + 
-  theme(axis.line.x = element_line(color="black", size = 1), #axis lines
-        axis.line.y = element_line(color="black", size = 1)) +
-  xlab(" ") + ylab(" ") + #axis labels
-  theme(text = element_text(colour = 'black', size = 14, face = 'bold')) + #label text
-  theme(panel.border = element_rect(colour = 'white',size = 1))+ #no border (white border)
-  theme(panel.border = element_rect(colour = 'white',size = 2))
+  theme(axis.line = element_line(color="black", size = 1)) +  # line at the axes
+  xlab(" ") + ylab(" ") +  # axis labels
+  theme(text = element_text(colour = 'black', size = 14, face = 'bold')) +  # label text
+  theme(panel.border = element_rect(colour = 'white',size = 1))  # no plot border (white border)
