1600f44e7aff20444e0fc4cb2d84bbceb8de238f
Sophia Wassermann <so.wassermann@gmail.com>
2017-11-16 14:29:27 +0000

    updated to 'classic' ggplot theme - my end goal with original script
    No gridlines, with axes.

--- ggplot_linegraph.R
+++ ggplot_linegraph.R
@@ -7,13 +7,11 @@ library(ggplot2)
 source("multiplot.R")  
 
 ggplot() + 
-  theme_bw() + 
+  theme_classic() + 
   geom_line(data= , aes(x= , y= ), colour="blue", size = 2) +  # line
   geom_point(data= , aes(x= , y= ), colour="black", size = 3) +  # points on the line
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
   theme(axis.text.y = element_text(size = 12),  # axis text size
         axis.text.x = element_text(size = 12)) + 
   theme(axis.line = element_line(color="black", size = 1)) +  # line at the axes
   xlab(" ") + ylab(" ") +  # axis labels
-  theme(text = element_text(colour = 'black', size = 14, face = 'bold')) +  # label text
-  theme(panel.border = element_rect(colour = 'white',size = 1))  # no plot border (white border)
+  theme(text = element_text(colour = 'black', size = 14, face = 'bold'))  # label text
